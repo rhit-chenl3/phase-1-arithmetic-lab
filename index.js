4c577f3d59b509e481b1201a0e390fa2e8445744
Leo Chen <chenl3@rose-hulman.edu>
2021-09-20 14:37:52 -0400

    commit tree 8964fc2e727dbc309c436ed40fdc4652a6571bad

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// function multiply(num1, num2){
+//     return 
+// }
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() + 1);
+
+const num3 = 9;
+const num4 = 5;
+const mod = 9 % 5;
+
+const set = (2, 3, 4, 5, 6, 7, 20);
+const max = Math.max(set);
